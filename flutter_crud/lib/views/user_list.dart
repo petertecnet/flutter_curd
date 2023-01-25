6d2164f44e6f56e0ee0a04ce67bde4f217daaea4
pivowstefanini <>
2023-01-25 13:27:07 -0300

    First

---
+++ flutter_crud/lib/views/user_list.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/components/user_tile.dart';
+import 'package:flutter_crud/data/dummy_users.dart';
+
+class UserList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final users = {...DUMMY_USERS};
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de usuarios'),
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (ctx, i) => UserTile(users.values.elementAt(i)),
+      ),
+    );
+  }
+}
